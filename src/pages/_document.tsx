1ee2c94af96794bfe4571f143566e03b1e762615
PhymasSC <garylau@live.com>
2023-04-28 13:08:31 +0800

    Improve SEO

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,6 +1,6 @@
 // pages/_document.js
-
 import React from 'react'
+
 import Document, {
   Html,
   Head,
@@ -24,16 +24,30 @@ class MyDocument extends Document {
       <Html lang='en'>
         <Head>
           {CssBaseline.flush()}
-          <link rel='preconnect' href='https://fonts.googleapis.com' />
-          <link
-            rel='preconnect'
-            href='https://fonts.gstatic.com'
-            crossOrigin='anonymous'
+          <title>Phymas</title>
+          <meta name='title' content='Phymas' />
+          <meta
+            name='description'
+            content="Meet Lau Sheng Cher, an inquisitive computer science student and aspiring full-stack web developer seeking an internship opportunity. With a strong passion for programming and technology, Lau Sheng Cher brings fresh insights and a creative approach to problem-solving. Explore Lau Sheng Cher's impressive portfolio, showcasing innovative projects that demonstrate his potential as a valuable addition to your firm. Don't miss out on the opportunity to connect with this rising talent in the field of computer science. Browse Lau Sheng Cher's portfolio today and get in touch to start a conversation about how Lau Sheng Cher can contribute to your organization."
           />
-          <link
-            href='https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap'
-            rel='stylesheet'
+
+          <meta property='og:type' content='website' />
+          <meta property='og:url' content='https://phymas.vercel.app/' />
+          <meta property='og:title' content='Phymas' />
+          <meta
+            property='og:description'
+            content="Meet Lau Sheng Cher, an inquisitive computer science student and aspiring full-stack web developer seeking an internship opportunity. With a strong passion for programming and technology, Lau Sheng Cher brings fresh insights and a creative approach to problem-solving. Explore Lau Sheng Cher's impressive portfolio, showcasing innovative projects that demonstrate his potential as a valuable addition to your firm. Don't miss out on the opportunity to connect with this rising talent in the field of computer science. Browse Lau Sheng Cher's portfolio today and get in touch to start a conversation about how Lau Sheng Cher can contribute to your organization."
           />
+          <meta property='og:image' content='/img/og_img.png' />
+
+          <meta property='twitter:card' content='summary_large_image' />
+          <meta property='twitter:url' content='https://phymas.vercel.app/' />
+          <meta property='twitter:title' content='Phymas' />
+          <meta
+            property='twitter:description'
+            content="Meet Lau Sheng Cher, an inquisitive comsputer science student and aspiring full-stack web developer seeking an internship opportunity. With a strong passion for programming and technology, Lau Sheng Cher brings fresh insights and a creative approach to problem-solving. Explore Lau Sheng Cher's impressive portfolio, showcasing innovative projects that demonstrate his potential as a valuable addition to your firm. Don't miss out on the opportunity to connect with this rising talent in the field of computer science. Browse Lau Sheng Cher's portfolio today and get in touch to start a conversation about how Lau Sheng Cher can contribute to your organization."
+          />
+          <meta property='twitter:image' content='/img/og_img.png' />
         </Head>
         <body>
           <Main />
