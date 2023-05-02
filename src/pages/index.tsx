5d60663c053f8f548603a6063ecbd5ceb312b3a8
PhymasSC <garylau@live.com>
2023-05-02 15:15:45 +0800

    Add NextSeo to index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,13 +4,37 @@ import About from '@/components/About'
 import { Col, Text, Grid } from '@nextui-org/react'
 import Skills from '@/components/Skills'
 import Head from 'next/head'
+import { NextSeo } from 'next-seo'
 
 const Home = () => {
   return (
     <>
-      <Head>
-        <title>Phymas</title>
-      </Head>
+      <NextSeo
+        title='Phymas'
+        description="Meet Lau Sheng Cher, a passionate computer science student seeking an internship. With impressive projects showcasing his potential as a full-stack web developer, connect with this rising talent in tech today. Browse Lau Sheng Cher's portfolio and discover how he can add value to your organization."
+        openGraph={{
+          url: 'https://phymas.vercel.app/',
+          title: 'Phymas',
+          description:
+            "Meet Lau Sheng Cher, a passionate computer science student seeking an internship. With impressive projects showcasing his potential as a full-stack web developer, connect with this rising talent in tech today. Browse Lau Sheng Cher's portfolio and discover how he can add value to your organization.",
+          images: [
+            {
+              url: 'https://www.phymas.me/api/og',
+              width: 800,
+              height: 600,
+              alt: 'Phymas',
+            },
+          ],
+          site_name: 'Phymas',
+        }}
+        robotsProps={{
+          nosnippet: true,
+          notranslate: true,
+          noimageindex: true,
+          noarchive: true,
+        }}
+        themeColor='#0072F5'
+      />
       <Grid.Container md>
         <Grid>
           <Col
