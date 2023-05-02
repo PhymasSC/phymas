57c966c6ee20f8db766adb5184d5f3d66354005a
PhymasSC <garylau@live.com>
2023-05-02 15:09:25 +0800

    Package: Add next-seo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,6 +10,7 @@ import { useSSR } from '@nextui-org/react'
 import Footer from '@/components/Footer'
 import './../styles/globals.css'
 import Head from 'next/head'
+import { DefaultSeo } from 'next-seo'
 
 interface MyAppProps {
   Component: React.ComponentType
@@ -43,61 +44,32 @@ const App = ({ Component, pageProps }: MyAppProps) => {
   return (
     isBrowser && (
       <>
-        <Head>
-          <meta
-            name='viewport'
-            content='initial-scale=1.0, width=device-width'
-          />
-          <meta property='og:image' content='https://www.phymas.me/api/og' />
-
-          <meta name='title' content='Phymas' />
-          <meta
-            name='description'
-            content="
-        Meet Lau Sheng Cher, a passionate computer science student seeking an internship. With impressive projects showcasing his potential as a full-stack web developer, connect with this rising talent in tech today. Browse Lau Sheng Cher's portfolio and discover how he can add value to your organization."
-          />
-
-          <meta property='og:type' content='website' />
-          <meta property='og:url' content='https://phymas.vercel.app/' />
-          <meta property='og:title' content='Phymas' />
-          <meta
-            property='og:description'
-            content="
-        Meet Lau Sheng Cher, a passionate computer science student seeking an internship. With impressive projects showcasing his potential as a full-stack web developer, connect with this rising talent in tech today. Browse Lau Sheng Cher's portfolio and discover how he can add value to your organization."
-          />
-          <meta property='og:image' content='https://www.phymas.me/api/og' />
-
-          <meta property='twitter:card' content='summary_large_image' />
-          <meta property='twitter:url' content='https://phymas.vercel.app/' />
-          <meta property='twitter:title' content='Phymas' />
-          <meta
-            property='twitter:description'
-            content="
-            Meet Lau Sheng Cher, a passionate computer science student seeking an internship. With impressive projects showcasing his potential as a full-stack web developer, connect with this rising talent in tech today. Browse Lau Sheng Cher's portfolio and discover how he can add value to your organization."
-          />
-          <meta property='twitter:image' content='/img/og_img.png' />
-
-          <link
-            rel='apple-touch-icon'
-            sizes='180x180'
-            href='/apple-touch-icon.png'
-          />
-          <link
-            rel='icon'
-            type='image/png'
-            sizes='32x32'
-            href='/favicon-32x32.png'
-          />
-          <link
-            rel='icon'
-            type='image/png'
-            sizes='16x16'
-            href='/favicon-16x16.png'
-          />
-          <link rel='manifest' href='/site.webmanifest' />
-          <meta name='msapplication-TileColor' content='#0072F5' />
-          <meta name='theme-color' content='#0072F5'></meta>
-        </Head>
+        <DefaultSeo
+          title='Phymas'
+          description="Meet Lau Sheng Cher, a passionate computer science student seeking an internship. With impressive projects showcasing his potential as a full-stack web developer, connect with this rising talent in tech today. Browse Lau Sheng Cher's portfolio and discover how he can add value to your organization."
+          openGraph={{
+            url: 'https://phymas.vercel.app/',
+            title: 'Phymas',
+            description:
+              "Meet Lau Sheng Cher, a passionate computer science student seeking an internship. With impressive projects showcasing his potential as a full-stack web developer, connect with this rising talent in tech today. Browse Lau Sheng Cher's portfolio and discover how he can add value to your organization.",
+            images: [
+              {
+                url: 'https://www.phymas.me/api/og',
+                width: 800,
+                height: 600,
+                alt: 'Phymas',
+              },
+            ],
+            site_name: 'Phymas',
+          }}
+          robotsProps={{
+            nosnippet: true,
+            notranslate: true,
+            noimageindex: true,
+            noarchive: true,
+          }}
+          themeColor='#0072F5'
+        />
         <NextUIProvider>
           <NextThemesProvider
             defaultTheme='system'
